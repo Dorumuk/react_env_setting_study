a2eb9541d7794c30a53f1fcdeff30fe95d7f0410
Dorumuk <haulinef@gmail.com>
2021-02-02 18:35:44 +0900

    first commit

---
+++ config-overrides.js
@@ -0,0 +1,17 @@
+/*
+7 line: customize-cra가 지원해주는 addDecoratorsLegacy 를 통해 decorator문법을 사용할 수 있게 되었다.
+8 line: 우리는 mobx-react의 observer decorator를 App.js에 적용해 볼 것이다. 
+        customize-cra 문서에서 disableEsLint 플러그인을 같이 사용하라고 나타나있기 때문에 설정파일에 명시하였다.
+*/
+const { 
+  addDecoratorsLegacy, 
+  disableEsLint, 
+  override 
+} = require("customize-cra");
+  
+module.exports = {
+  webpack: override(
+      disableEsLint(),
+      addDecoratorsLegacy()
+  )
+};
